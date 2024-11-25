87fdb15bda4ff4d5e59f0f738bb29992c3094c91
Bell_Bell <47379176+3210439@users.noreply.github.com>
2024-11-25 22:32:26 +0900

    Update getServerUrl.ts

--- server/getServerUrl.ts
+++ server/getServerUrl.ts
@@ -1,4 +1,4 @@
 export const getServerUrl = (path: string) => {
-  const host = 'http://localhost:8080'
+  const host = 'http://ec2-54-180-251-225.ap-northeast-2.compute.amazonaws.com:8080'
   return [host, path].join('')
 }
